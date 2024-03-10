ae799457d76ca6322e10b7d2a38b4e5ae653925f
MarcosFriass <marcos.frias.00@gmail.com>
2024-03-10 21:02:57 +0100

    RAFT i ef-RAFT

---
+++ W3/Ef-RAFT/README.md
@@ -0,0 +1,76 @@
+# Ef-RAFT
+ 	
+[![PWC](https://img.shields.io/endpoint.svg?url=https://paperswithcode.com/badge/rethinking-raft-for-efficient-optical-flow/optical-flow-estimation-on-sintel-clean)](https://paperswithcode.com/sota/optical-flow-estimation-on-sintel-clean?p=rethinking-raft-for-efficient-optical-flow)
+
+
+ 	
+[![PWC](https://img.shields.io/endpoint.svg?url=https://paperswithcode.com/badge/rethinking-raft-for-efficient-optical-flow/optical-flow-estimation-on-kitti-2015-train)](https://paperswithcode.com/sota/optical-flow-estimation-on-kitti-2015-train?p=rethinking-raft-for-efficient-optical-flow)
+
+ 	
+[![PWC](https://img.shields.io/endpoint.svg?url=https://paperswithcode.com/badge/rethinking-raft-for-efficient-optical-flow/optical-flow-estimation-on-sintel-final)](https://paperswithcode.com/sota/optical-flow-estimation-on-sintel-final?p=rethinking-raft-for-efficient-optical-flow)
+
+This repository contains the source code for [Ef-RAFT: Rethinking RAFT for Efficient Optical Flow](https://arxiv.org/abs/2401.00833)<br/>
+
+<img src="Diagram.png">
+
+## Requirements
+The code has been tested with PyTorch 1.6 and Cuda 10.1.
+```Shell
+conda create --name efraft
+conda activate raft
+conda install pytorch=1.6.0 torchvision=0.7.0 cudatoolkit=10.1 matplotlib tensorboard scipy opencv -c pytorch
+```
+
+
+## Required Data
+To evaluate/train RAFT, you will need to download the required datasets and put them in ```datasets/``` directory. 
+* [FlyingChairs](https://lmb.informatik.uni-freiburg.de/resources/datasets/FlyingChairs.en.html#flyingchairs)
+* [FlyingThings3D](https://lmb.informatik.uni-freiburg.de/resources/datasets/SceneFlowDatasets.en.html)
+* [Sintel](http://sintel.is.tue.mpg.de/)
+* [KITTI](http://www.cvlibs.net/datasets/kitti/eval_scene_flow.php?benchmark=flow)
+
+## How to run?
+For training on 2 GPUs, run the following command. Training logs will be written to the `runs` directory, which can be visualized using tensorboard.
+```Shell
+./train_standard.sh
+```
+
+For running on a single RTX GPU, training can be accelerated using mixed precision, and can be done with the following command. You can expect similiar results in this setting (1 GPU).
+```Shell
+./train_mixed.sh
+```
+
+You can evaluate a trained model using `evaluate.py`.
+```Shell
+python evaluate.py --model=models/raft-things.pth --dataset=sintel --mixed_precision
+```
+
+### Quantitative Results
+Comparison of the proposed method with existing
+techniques on the Sintel and KITTI datasets. Metrics in green, blue, and
+red denote the first, second, and third-best results, respectively.
+<p align="center">
+<img src="Results.png" width="700" height="500">
+<p/>
+
+
+### Qualitative Results
+Qualitative comparison between the proposed method and RAFT. Frames with orange and blue labels are from Sintel
+and KITTI datasets, respectively.
+<img src="Visualization.png">
+
+
+ ## Citation
+If you use this repository for your research or wish to refer to our method, please use the following BibTeX entry:
+```bibtex
+@article{eslami2024rethinking,
+  title={Rethinking RAFT for Efficient Optical Flow},
+  author={Eslami, Navid and Arefi, Farnoosh and Mansourian, Amir M and Kasaei, Shohreh},
+  journal={arXiv preprint arXiv:2401.00833},
+  year={2024}
+}
+```
+
+### Acknowledgement
+This codebase is heavily borrowed from [RAFT: Recurrent All Pairs Field Transforms for Optical Flow](https://github.com/princeton-vl/RAFT). Thanks for their excellent work.
+
---
+++ W3/RAFT/demo.py
@@ -0,0 +1,122 @@
+import sys
+sys.path.append('core')
+
+import argparse
+import os
+import cv2
+import glob
+import numpy as np
+import torch
+from PIL import Image
+
+from raft import RAFT
+from utils import flow_viz
+from utils.utils import InputPadder
+
+
+
+DEVICE = 'cpu'
+
+def load_image(imfile):
+    img = np.array(Image.open(imfile)).astype(np.uint8)
+
+    # convert gray to 3 channels
+    if len(img.shape) == 2:
+        img = np.stack([img, img, img], axis=2)
+    img = torch.from_numpy(img).permute(2, 0, 1).float()
+    return img[None].to(DEVICE)
+
+
+def viz(img, flo):
+    img = img[0].permute(1,2,0).cpu().numpy()
+    flo = flo[0].permute(1,2,0).cpu().numpy()
+    
+    # map flow to rgb image
+    flo = flow_viz.flow_to_image(flo)
+    img_flo = np.concatenate([img, flo], axis=0)
+
+    # import matplotlib.pyplot as plt
+    # plt.imshow(img_flo / 255.0)
+    # plt.show()
+
+    cv2.imshow('image', img_flo[:, :, [2,1,0]]/255.0)
+    cv2.waitKey()
+
+def save_flow_to_image(u, v, valid, filename):
+    assert u.shape == v.shape == valid.shape, "Mismatch in dimension of flow components and valid mask"
+    
+    # Initialize the flow image with all invalid pixels
+    flow_img = np.zeros((u.shape[0], u.shape[1], 3), dtype=np.uint16)
+    
+    # Set the flow values, scaling and encoding as per KITTI format
+    flow_img[..., 0] = (u * 64.0 + 2**15).astype(np.uint16)
+    flow_img[..., 1] = (v * 64.0 + 2**15).astype(np.uint16)
+    flow_img[..., 2] = valid
+    
+    flow_img_bgr = np.stack((flow_img[:, :, 2], flow_img[:, :, 1], flow_img[:, :, 0]), axis=-1)
+    # Save the image
+    cv2.imwrite(filename, flow_img_bgr)
+
+
+def demo(args):
+    model = torch.nn.DataParallel(RAFT(args))
+    model.load_state_dict(torch.load(args.model, map_location=torch.device('cpu')))
+
+    model = model.module
+    model.to(DEVICE)
+    model.eval()
+
+    with torch.no_grad():
+        images = glob.glob(os.path.join(args.path, '*.png')) + \
+                 glob.glob(os.path.join(args.path, '*.jpg'))
+        
+        images = sorted(images)
+        for imfile1, imfile2 in zip(images[:-1], images[1:]):
+            image1 = load_image(imfile1)
+            image2 = load_image(imfile2)
+
+
+
+            padder = InputPadder(image1.shape)
+            image1, image2 = padder.pad(image1, image2)
+
+            flow_low, flow_up = model(image1, image2, iters=20, test_mode=True)
+
+
+            flo = flow_up[0].permute(1,2,0).cpu().numpy()
+
+            # save flow to image
+            save_flow_to_image(flo[:,:,0], flo[:,:,1], np.ones_like(flo[:,:,0]), 'flow.png')
+
+            # open png image from GT folder
+            #gt = load_image(os.path.join('GT', '000045_10.png'))
+
+
+            gt = np.array(Image.open(os.path.join('GT', '000045_10.png'))).astype(np.uint8)
+
+            import pdb; pdb.set_trace()
+            # remove last channel
+            gt = gt[:,:,:2]
+
+
+            gt = torch.from_numpy(gt).permute(2, 0, 1).float()
+            
+            # convert gt size from [2, H, W] to [1, 2, H, W]
+            gt = gt[None].to(DEVICE)
+
+            padder = InputPadder(gt.shape)
+            gt= padder.pad(gt) 
+
+            viz(image1, gt[0])
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', help="restore checkpoint")
+    parser.add_argument('--path', help="dataset for evaluation")
+    parser.add_argument('--small', action='store_true', help='use small model')
+    parser.add_argument('--mixed_precision', action='store_true', help='use mixed precision')
+    parser.add_argument('--alternate_corr', action='store_true', help='use efficent correlation implementation')
+    args = parser.parse_args()
+
+    demo(args)
