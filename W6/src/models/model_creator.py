34c5be14267c65eb88d64f3017c7b8ac31389a58
angelhd1999 <angelhd1999@gmail.com>
2024-04-21 16:34:07 +0200

    Updated models without movinet

--- W6/src/models/model_creator.py
+++ W6/src/models/model_creator.py
@@ -2,10 +2,30 @@
 
 import torch
 import torch.nn as nn
+import torchvision
+
+# from movinets import MoViNet
+# from movinets.config import _C
 
 def create(model_name: str, load_pretrain: bool, num_classes: int) -> nn.Module:
     if model_name == 'x3d_xs':
         return create_x3d_xs(load_pretrain, num_classes)
+    
+    # elif model_name == 'movinet_a0_stream':
+    #     return create_movinet_a0_stream(load_pretrain, num_classes)
+
+    elif model_name == 'mobilenetV3_small':
+        return create_mobilenetV3_small(load_pretrain, num_classes)
+    
+    elif model_name == 'efficientnet_b0':
+        return create_efficientnet_b0(load_pretrain, num_classes)
+
+    elif model_name == 'squeezenet':
+        return create_squeezenet(load_pretrain, num_classes)
+
+    elif model_name == 'shufflenet_v2':
+        return create_shufflenet_v2(load_pretrain, num_classes)
+    
     else:
         raise ValueError(f"Model {model_name} not supported")
 
@@ -16,4 +36,30 @@ def create_x3d_xs(load_pretrain, num_classes):
         model,
         nn.Linear(2048, num_classes, bias=True),
     )
-    
+
+# def create_movinet_a0_stream(load_pretrain, num_classes):
+#     # load movinet A0 Stream
+#     model = MoViNet(_C.MODEL.MoViNetA0,pretrained = True)
+#     return model
+
+def create_mobilenetV3_small(load_pretrain, num_classes):
+    model = torchvision.models.mobilenet_v3_small(pretrained=load_pretrain)
+    model.classifier[3] = nn.Linear(1024, num_classes)
+    return model
+
+def create_efficientnet_b0(load_pretrain, num_classes):
+    model = torchvision.models.efficientnet_b0(pretrained=load_pretrain)
+    num_ftrs = model._fc.in_features
+    model._fc = nn.Linear(num_ftrs, num_classes)
+    return model
+
+def create_squeezenet(load_pretrain, num_classes):
+    model = torchvision.models.squeezenet1_1(pretrained=load_pretrain)
+    model.classifier[1] = nn.Conv2d(512, num_classes, kernel_size=(1, 1), stride=(1, 1))
+    model.num_classes = num_classes
+    return model
+
+def create_shufflenet_v2(load_pretrain, num_classes):
+    model = torchvision.models.shufflenet_v2_x1_5(pretrained=load_pretrain)
+    model.fc = nn.Linear(1024, num_classes)
+    return model
