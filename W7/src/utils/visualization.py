a8b0f8d474f5d294b3d3b7f6364e5ab974e177d8
franmuline <franmb2000@gmail.com>
2024-04-30 23:16:14 +0200

    W7 - Changes in visualization.py to admit relative improvement

--- W7/src/utils/visualization.py
+++ W7/src/utils/visualization.py
@@ -1,14 +1,28 @@
 import matplotlib.pyplot as plt
 
 
-def plot_acc_per_class(acc_per_class: dict, save_path=None):
+def plot_acc_per_class(acc_per_class: dict, save_path=None, relative=False):
     plt.figure(figsize=(12, 6))
-    plt.bar(acc_per_class.keys(), acc_per_class.values())
+    if not relative:
+        plt.bar(acc_per_class.keys(), acc_per_class.values())
+    else:
+        for i, v in zip(acc_per_class.keys(), acc_per_class.values()):
+            if v > 0:
+                plt.bar(i, v, color='g')
+            else:
+                plt.bar(i, v, color='r')
+
     plt.xticks(rotation=90)
     plt.ylabel('Accuracy')
     plt.xlabel('Class')
-    plt.title('Accuracy per class')
-    plt.ylim(0, 1)
+    if not relative:
+        plt.title('Accuracy per class')
+    else:
+        plt.title('Relative improvement per class')
+    if not relative:
+        plt.ylim(0, 1)
+    else:
+        plt.ylim(-1, 1)
     plt.subplots_adjust(bottom=0.3)
     if save_path is not None:
         plt.savefig(save_path)
